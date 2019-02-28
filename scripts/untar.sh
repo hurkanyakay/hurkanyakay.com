e75df101f9ede3716ab4a88ea0edcb68e0ba0f71
Hürkan Yakay <hyakay@gmail.com>
2019-02-28 12:08:53 +0300

    travis deploy test3

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 set -x
-
-#export NODE_ENV=production
-#export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
-
-cd /var/www/html/hurkanyakay.com/travisbuild/ && \
-tar zxvf package.tgz -C . && \
+cd /var/www/html/hurkanyakay.com/travisbuild && \
+cd /var/www/html/hurkanyakay.com/travisbuild && \
+rm -r public && \
+tar zxvf package.tgz -C .

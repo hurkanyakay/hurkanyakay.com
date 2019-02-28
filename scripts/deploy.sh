b7fa01a53f595aa045f83c2ee4f2674aa67b1fb3
Hürkan Yakay <hyakay@gmail.com>
2019-02-28 13:59:34 +0300

    travis deploy test6

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -x
 
+cd scripts && \
 tar -czf package.tgz -C ../ public && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./untar.sh

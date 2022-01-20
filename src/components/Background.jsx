b1b1b8323f9e4672dd29dab5ab4c1d4a722efcfe
Hürkan Yakay <hyakay@gmail.com>
2022-01-20 19:24:34 +0300

    particles

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,126 +1,67 @@
 import React from 'react';
-import Particles from 'react-particles-js';
+import Particles from 'react-tsparticles';
 import Img from 'gatsby-image';
 import { ParticleWrapper } from './LayoutComponents';
+
+const particleParams = {
+  fullScreen: {
+    enable: false
+  },
+  fpsLimit: 30,
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 3,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 20,
+    },
+    opacity: {
+      value: 0.3,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+};
 
 const color = '#75A5B7';
 const maxParticles = 80;
 export default class Background extends React.Component {
   shouldComponentUpdate() {
-    return false;
+    return true;
   }
 
   render() {
     return (
       <ParticleWrapper>
         <Img className="background" fluid={this.props.data.childImageSharp.fluid} />
-        <Particles
-          params={{
-            particles: {
-              number: {
-                value: maxParticles,
-                density: {
-                  enable: true,
-                  value_area: maxParticles * 10 * 2,
-                },
-              },
-              color: {
-                value: color,
-              },
-              shape: {
-                type: 'circle',
-                stroke: {
-                  width: 0,
-                  color: '#000000',
-                },
-                polygon: {
-                  nb_sides: 5,
-                },
-              },
-              opacity: {
-                value: 0.5,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false,
-                },
-              },
-              size: {
-                value: 3,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 40,
-                  size_min: 0.1,
-                  sync: false,
-                },
-              },
-              line_linked: {
-                enable: true,
-                distance: 150,
-                color,
-                opacity: 1,
-                width: 1,
-              },
-              move: {
-                enable: true,
-                speed: 2,
-                direction: 'none',
-                random: false,
-                straight: false,
-                out_mode: 'out',
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200,
-                },
-              },
-              interactivity: {
-                detect_on: 'canvas',
-                events: {
-                  onhover: {
-                    enable: true,
-                    mode: 'grab',
-                  },
-                  onclick: {
-                    enable: true,
-                    mode: 'push',
-                  },
-                  resize: true,
-                },
-                modes: {
-                  grab: {
-                    distance: 140,
-                    line_linked: {
-                      opacity: 1,
-                    },
-                  },
-                  bubble: {
-                    distance: 400,
-                    size: 40,
-                    duration: 2,
-                    opacity: 8,
-                    speed: 3,
-                  },
-                  repulse: {
-                    distance: 200,
-                    duration: 0.4,
-                  },
-                  push: {
-                    particles_nb: 4,
-                  },
-                  remove: {
-                    particles_nb: 2,
-                  },
-                },
-              },
-              retina_detect: true,
-            },
-          }}
-        />
+        <Particles className="particles" options={particleParams} />
       </ParticleWrapper>
     );
   }
